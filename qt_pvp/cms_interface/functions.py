53e836e872cf91183a4b5b49e0fcb58f30e475a8
Your Name <you@example.com>
2025-07-01 15:09:10 +0500

    Теперь начало видео совпадает с фото до а конец видео с фото после

--- qt_pvp/cms_interface/functions.py
+++ qt_pvp/cms_interface/functions.py
@@ -217,8 +217,10 @@ def find_by_lifting_switches(tracks, sec_before=30, sec_after=30):
                 logger.info(f"[INTEREST] Интерес от {time_before} до {time_after}")
                 interval = get_interest_from_track(
                     tracks[-1],
-                    start_time=time_30_before_dt.strftime("%Y-%m-%d %H:%M:%S"),
-                    end_time=time_30_after,
+                    start_time=time_before,
+                    #start_time=time_30_before_dt.strftime("%Y-%m-%d %H:%M:%S"),
+                    end_time=time_after,
+                    #end_time=time_30_after,
                     photo_before_timestamp=time_before,
                     photo_after_timestamp=time_after
                 )
