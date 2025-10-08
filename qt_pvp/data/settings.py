577fb668a59895f8eeefe4c9725ab905205583c2
Your Name <you@example.com>
2025-10-08 15:05:27 +0500

    Добавил игнорирование ДЕПО, мпз и тд. Изменил немного структуру директории

--- qt_pvp/data/settings.py
+++ qt_pvp/data/settings.py
@@ -17,23 +17,15 @@ INTERESTING_VIDEOS_FOLDER = os.path.join(CUR_DIR, "interesting_videos")
 TESTS_MISC_FOLDER = os.path.join(TESTS_FOLDER, "misc")
 IGNORE_POINTS_JSON = os.path.join(DATA_FOLDER, "ignore_points.json")
 LOGS_DIR = os.path.join(CUR_DIR, "logs")
-CONFIG_PATH = os.path.join(CUR_DIR, "config.cfg")
+CONFIG_PATH = os.path.join(DATA_FOLDER, "config.cfg")
 CLOUD_PATH = posixpath.join("/Tracker", "Видео выгрузок")
+states = os.sep.join((DATA_FOLDER, "states.json"))
 
 config = configparser.ConfigParser(
     inline_comment_prefixes='#',
     allow_no_value=True)
 config.read(CONFIG_PATH, encoding="utf-8")
 
-states = os.sep.join((CUR_DIR, "states.json"))
-
-eumid_host = f"http://{config.get('eumid', 'ip')}:" \
-             f"{config.get('eumid', 'port')}/v1"
-get_video_rout = f"{eumid_host}/video"
-get_devices_rout = f"{eumid_host}/devices"
-get_devices_online = f"{eumid_host}/analyze/online"
-get_alarm_analyze = f"{eumid_host}/analyze/by_alarm"
-add_download_task = f"{eumid_host}/StandardVideoTrackAction_addDownloadTask.action"
 
 cms_host = f"{config.get('CMS', 'schema')}{config.get('CMS', 'ip')}:" \
            f"{config.getint('CMS', 'port')}"
